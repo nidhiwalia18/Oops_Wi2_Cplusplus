2fa46be428103abd5cdafe82a776e8119ff69d76
nidhi walia <nidhiw1810@gmail.com>
2022-12-04 14:09:08 +0530

    Update 007StaticMemberfunctionPr5_5.cpp

--- AssignmentOOP/007StaticMemberfunctionPr5_5.cpp
+++ AssignmentOOP/007StaticMemberfunctionPr5_5.cpp
@@ -1,36 +1,24 @@
-#include<iostream>
+//program to show the use of static member function showcount in a class
+#include <iostream>
 using namespace std;
-
-class test
+class abc
 {
-	private:
-		int code;
-		static int count;	//Static member variable or class variable
-	public:
-		void setcode(void){code = ++count;}
-		void showcode(void){cout<<"Object number: "<<code<<"\n";}
-		static void showcount(void){cout<<"count: "<<count<<"\n";}
+private:
+ static int showcount; //static member function
+public:
+	abc()
+	{
+		showcount++;
+	}
+	void display()
+	{
+		cout<<"the number of objects created = "<<showcount;
+	}
+	
 };
-
-int test :: count;
-
+int abc :: showcount = 0;
 int main()
 {
-	test t1,t2;
-	
-	t1.setcode();
-	t2.setcode();
-	
-	test :: showcount();	//accessing static function Without using dot operator static member function can be called 
-	
-	test t3;
-	t3.setcode();
-	
-	test :: showcount();
-	
-	t1.showcode();
-	t2.showcode();
-	t3.showcode();
-	
-	return 0;
+	abc c1,c2,c3,c4,c5;
+	c1.display();
 }
